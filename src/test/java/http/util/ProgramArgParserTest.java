922d4962cb1b32b566882557fada808b3049abc4
Devlin Glasman <devlin.glasman@gmail.com>
2018-09-20 14:11:56 +0100

    Refactors programargparser tests.

--- src/test/java/http/util/ProgramArgParserTest.java
+++ src/test/java/http/util/ProgramArgParserTest.java
@@ -7,43 +7,21 @@ import static junit.framework.TestCase.assertEquals;
 public class ProgramArgParserTest {
 
     @Test
-    public void givenPortNumberArg_setPort() {
-        String portArg = "5000";
-        int port = Integer.parseInt(portArg);
-        String[] programArgs = {"-p", portArg};
-
-        ProgramArgParser programArgParser = new ProgramArgParser(programArgs);
-
-        assertEquals(port, programArgParser.getPort());
-    }
-
-    @Test
-    public void givenDirArg_setRootDir() {
-        String dirArg = "src/test/resources/";
-        String[] programArgs = {"-d", dirArg};
-
-        ProgramArgParser programArgParser = new ProgramArgParser(programArgs);
-
-        assertEquals(dirArg, programArgParser.getRootPath());
-    }
-
-    @Test
     public void givenPortAndDirArgs_setBoth() {
         String portArg = "5000";
-        int port = Integer.parseInt(portArg);
         String dirArg = "src/test/resources/";
         String[] programArgs = {"-p", portArg, "-d", dirArg};
 
         ProgramArgParser programArgParser = new ProgramArgParser(programArgs);
 
-        assertEquals(port, programArgParser.getPort());
+        assertEquals(Integer.parseInt(portArg), programArgParser.getPort());
         assertEquals(dirArg, programArgParser.getRootPath());
     }
 
     @Test
-    public void givenNoPort_setDefault() {
-        String[] programArgs = {};
-        ProgramArgParser programArgParser = new ProgramArgParser(programArgs);
+    public void givenNoPort_defaultExists() {
+        String[] emptyArgs = {};
+        ProgramArgParser programArgParser = new ProgramArgParser(emptyArgs);
 
         assertEquals(5000, programArgParser.getPort());
     }
